b90375a1d68a365f1d8aef65b75e8f2d8fd6d4d6
Admin <airstage@mail.ru>
2023-04-21 16:40:11 +0300

    Added a new function.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from math import sqrt
+
+
+def hypotinuse(a, b):
+    return sqrt(a ** 2 + b ** 2)
+
+
 print('Hello, this is my first script!')
 
 print('My name is Kirill.')
 print('I`m gonna add a couple functions in my programm.')
 
 print('The end!')
+print(hypotinuse(4, 3))
